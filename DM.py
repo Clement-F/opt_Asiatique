3ff46b0a8a7f6a08c44fd3883258ff9d4e28d597
Clement <clement.fontan@outlook.com>
2025-03-27 11:47:58 +0100

    début du DM

--- DM.py
+++ DM.py
@@ -4,7 +4,7 @@ import scipy.stats as ss
 
 #================= definition brownian ======================
 def brownian(T,N):
-    Xi = np.random.uniform(N)
+    Xi = np.random.uniform(size = N)
     Ni = ss.norm(0,T/N).ppf(Xi)
     WT = sum(Ni)
     return WT
@@ -17,14 +17,16 @@ def Brown_traj(T,N):
     for k in range (0,N):
         W.append(W[k]+Ni[k])
     return W
-T = 5
-N= 50
-W = Brown_traj(T,N)
-t = np.linspace(0,T,N+1)
+T = 50
+N= 1000*T
+# W = Brown_traj(T,N)
+# t = np.linspace(0,T,N+1)
 
-print(W)
-plt.plot(t,W)
-plt.show()
+# print(brownian(T,N))
+
+# print(W)
+# plt.plot(t,W)
+# plt.show()
 #================= definition fonctions =====================
 T=1
 r=1
@@ -35,13 +37,26 @@ M2 = (2*np.exp((2*r+s*s)*T))/((r+s*s)*(2*r+s*s)*T*T) + 2/(r*T*T) * (1/(2*r + s*s
 rA = np.log(M1)/T
 sA = np.sqrt(np.log(M2)/T - 2*rA)
 
-s0 = 0
+s0 = 1
 eps = 1
 
-def SA_t(t,brownt):
-    s = s0 * np.exp((rA - sA*sA/2 )*t + sA * brownt )
+def SA_t(t):
+    s = s0 * np.exp((rA - sA*sA/2 )*t + sA * brownian(t,10*t) )
     return s
 
-def P0TW(T):
-    q1 = np.exp(-rA*T) 
-    return q1
+def P0TW(T,N,K):
+
+    S = []
+    for k in range(1,N):
+        S.append(max(0, SA_t(T)-K))
+
+    q1 = np.exp(-rA*T)
+    q2 =  1/N * eps* np.sum(S)
+    return q1*q2
+
+T = 10
+N = 20
+K = 2
+
+for k in range(10):
+    print(P0TW(T,N,K))
