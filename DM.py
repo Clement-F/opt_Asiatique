2e0a1c01c45d564b7f99a61b68fb719f58096ee7
Clement <clement.fontan@outlook.com>
2025-03-29 02:07:14 +0100

    essaie basé sur le papier (raté)

--- DM.py
+++ DM.py
@@ -29,15 +29,15 @@ N= 1000*T
 # plt.show()
 #================= definition fonctions =====================
 T=1
-r=1
-s=1
+r=100
+s=0.2
 
 M1 = (np.exp(r*T)-1)/(r*T)
 M2 = (2*np.exp((2*r+s*s)*T))/((r+s*s)*(2*r+s*s)*T*T) + 2/(r*T*T) * (1/(2*r + s*s) - np.exp(r*T)/(r+s*s))
 rA = np.log(M1)/T
 sA = np.sqrt(np.log(M2)/T - 2*rA)
 
-s0 = 1
+s0 = 100
 eps = 1
 
 def SA_t(t):
@@ -54,12 +54,22 @@ def P0MC(T,N,K):
     q2 =  1/N * eps* np.sum(S)
     return q1*q2
 
-# def P0TW(T,N,d,K):
-#     m = np.log(s0) + (rA -)
 
-T = 10
-N = 5
-K = 2
 
-# for k in range(10):
-#     print(P0TW(T,N,K))
+def P0TW(T,N,K):
+    m = np.log(s0) + (rA - sA*sA/2)*(T-N) + (rA - sA*sA/2)*(N+1)/2
+    s = sA * sA * (T-N) * sA*sA * ( (N+1)*(2*N+1)/6)/N
+
+    d = -np.log(K)/s + m/s +s
+
+    P = np.exp(-rA*T) * ((np.exp(m+s*s))*ss.norm.cdf(d) - K *ss.norm.cdf(d-s) )
+    return P
+
+T = 100
+N = T-10
+K = 100
+
+
+
+for k in range(10):
+     print(P0TW(T,N,K))
