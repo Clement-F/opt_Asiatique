6fc2c47fd7f23f087a95b2f4d39f1778904c1cb6
Clement <clement.fontan@outlook.com>
2025-04-17 11:51:44 +0200

    Q4

--- DM.py
+++ DM.py
@@ -62,6 +62,9 @@ def P0TW(T,K,s0,r,sig):
     M2 = (2*np.exp((2*r+sig*sig)*T))/((r+sig*sig)*(2*r+sig*sig)*T*T) + 2/(r*T*T) * (1/(2*r + sig*sig) - np.exp(r*T)/(r+sig*sig))
     rA = np.log(M1)/T
     sigA = np.sqrt(np.log(M2)/T - 2*rA)
+    # print(M1,M2)
+    # print(rA,sigA)
+    # print(np.log(M2)/T - 2*rA)
 
     d = 1/(sigA*np.sqrt(T))*( np.log(s0/K) + rA*T)
 
@@ -71,19 +74,19 @@ def P0TW(T,K,s0,r,sig):
 
 def P0TWD(T,K,s0,r,sig,N):
     dt = T/N
-    M1 = 1/N * (np.exp(r*dt)*(1-np.exp(r*N*dt)/(1-np.exp(r*dt))))
+    M1 = 1/N * (np.exp(r*dt)*(1-np.exp(r*N*dt))/(1-np.exp(r*dt)))
     M21 = 1/N**2 * (np.exp((2*r+sig*sig)*dt) * (1-np.exp((2*r+sig*sig)*dt*N))/(1-np.exp((2*r+sig*sig)*dt)))
     M22 = 1/N**2 * (  2*np.exp(r*dt)/(1-np.exp(r*dt))   *   (np.exp((2*r+sig*sig)*dt) * (1-np.exp((2*r+sig*sig)*(N-1)*dt))/(1-np.exp((2*r+sig*sig)*dt)) - (np.exp(((N+1)*r+sig*sig)*dt) * (1-np.exp((r+sig*sig)*(N-1)*dt))/(1-np.exp((r+sig*sig)*dt))))  )
     M2 = M21 + M22
     rA = np.log(M1)/T
     sigA = np.sqrt(np.log(M2)/T - 2*rA)
-    print(M1,M2)
-    print(rA,sigA)
-    print(np.log(M2)/T - 2*rA)
+    # print(M1,M2)
+    # print(rA,sigA)
+    # print(np.log(M2)/T - 2*rA)
     
     d = 1/(sigA*np.sqrt(T))*( np.log(s0/K) + rA*T)
 
-    P = np.exp(-rA*T) * (s0*(ss.norm.cdf(d + (sigA*np.sqrt(T))*(sigA**2/2 *T))) - K *(ss.norm.cdf(d - (sigA*np.sqrt(T))*(sigA**2/2 *T) )))
+    P = np.exp(-r*T) * (s0* np.exp(rA*T)*(ss.norm.cdf(d + (sigA*np.sqrt(T))*(sigA**2/2 *T))) - K *(ss.norm.cdf(d - (sigA*np.sqrt(T))*(sigA**2/2 *T) )))
     return P
 
 s0=1
@@ -93,17 +96,17 @@ r=0.01
 sig = 0.3
 N=252*T
 
-print(P0TWD(T,K,s0,r,sig,T))
+# print(P0TWD(T,K,s0,r,sig,N))
+# print(P0TW(T,K,s0,r,sig))
 
 
-
-# t = np.linspace(T,0,2*T,endpoint=False)
-# P1 = P0TWD(t,K,s0,r,sig,N)
-# # P2 = P0TW(t,K,s0,r,sig)
-# X = sig*np.sqrt(t)/2
-# print(P1)
-# plt.plot(t,P1,label='price 2')
-# # plt.plot(t,P2,label='price')
-# plt.plot(t,X,label='esti')
-# plt.legend()
-# plt.show()
+t = np.linspace(T,0,2*T,endpoint=False)
+P1 = P0TWD(t,K,s0,r,sig,N)
+P2 = P0TW(t,K,s0,r,sig)
+X = sig*np.sqrt(t)/2
+print(P1)
+plt.plot(t,P1,label='price 2')
+plt.plot(t,P2,label='price')
+plt.plot(t,X,label='esti')
+plt.legend()
+plt.show()
