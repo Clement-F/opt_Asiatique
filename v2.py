d8ff4b9d8c02438f89b00e93da000328f23744fa
Clement <clement.fontan@outlook.com>
2025-04-29 16:45:41 +0200

    Q8

--- v2.py
+++ v2.py
@@ -104,7 +104,7 @@ def Brown_traj(T,N,s=1):
 # plt.plot(t,W)
 # plt.show()
 
-def Asian_options(r,S0,K,sig,N,T,n):
+def Pdt_MC(r,S0,K,sig,N,T,n):
     
     LR = (r-sig**2/2)*T/n + sig*np.sqrt(T/n) * ss.norm(0,1).rvs((N,n))  #on simule les n morceaux des N trajectoire que prenne les logarithme des cours de l'action
     LRD = np.concatenate((np.ones((N,1))*np.log(S0),LR),axis=1)         #on ajoute le départ log(S0)
@@ -134,7 +134,7 @@ def Asian_options(r,S0,K,sig,N,T,n):
 # N=10**4
 # n=10
 
-# res = Asian_options(r, so, K, s, N, T,n)
+# res = Pdt_MC(r, so, K, s, N, T,n)
 # print("le prix estimé par MC est : ", res[0])
 # print("la certitude est de 95%")
 # print("l'intervalle de confiance à 95% est : [",res[4],",",res[3],"]")
@@ -148,12 +148,48 @@ def Asian_options(r,S0,K,sig,N,T,n):
 ####################################
 
 
-
+def Pdt_MC_ctrl(r,S0,K,sig,N,T,n):
+    1+1
+    return 0,0,0,0,0
 
 #####################################
 # Q8 : comparaison des estimations
 ####################################
 
+
+# r=0.04
+# K=so=100
+# s=0.2
+# T=1
+# n_traj = 10
+
+# n=10
+# sample = [2**k for k in range(n)]
+
+# P = np.zeros((2,n))
+# err = np.zeros((2,n))
+# up = np.zeros((2,n))
+# down = np.zeros((2,n))
+
+# lorem = 0
+
+# for i in range(n) :
+#     [P[0,i],lorem,err[0,i],up[0,i],down[0,i]] = Pdt_MC(r, so, K, s, sample[n], T,n_traj)
+#     [P[1,i],lorem,err[1,i],up[1,i],down[1,i]] = Pdt_MC_ctrl(r, so, K, s, sample[n], T,n_traj)
+
+# plt.plot(sample,P[0])
+# plt.plot(sample,P[1])
+# plt.show()
+
+# plt.plot(sample,err[0])
+# plt.plot(sample,err[1])
+# plt.show()
+
+# plt.plot(sample,up[0])
+# plt.plot(sample,up[1])
+# plt.plot(sample,down[0])
+# plt.plot(sample,down[1])
+# plt.show()
 
 #####################################
 # Q9 : 
@@ -170,22 +206,27 @@ r = 0.01
 sig = 0.3
 n=10         #nombre d'échantillons du strike 
 
-detail = [252,52,12]
+detail = [int(1e+6),252,52,12]
 K = np.linspace(2,0,n,endpoint=False)
-P = np.zeros((4,n))
+P = np.zeros((5,n))
 
 for k in range(n):
     P[0,k] = ( P0_TW(T, K[k], s0, r, sig))
     print("pour dt = 1, le prix estimé par TW est :",P[0,k])
 
-    for d in range(3) : 
+    for d in range(4) : 
         N = detail[d]*T
         P[d+1,k] = PD_TW(T, K[k], s0, r, sig, N)  
         t = np.linspace(T, 0, detail[d]*T, endpoint=False)
         print("pour dt = 1/",detail[d]," le prix estimé par TW est :",P[d+1,k])
 
-for k in range(4):
-    plt.plot(K,P[k])
+plt.loglog(K,P[0]-P[1],label=0)
+for k in range(2,5):
+    plt.loglog(K,P[k]-P[1],label=k)
 
+plt.legend()
 plt.grid()
+plt.show()
+
+plt.plot(K,P[0])
 plt.show()
